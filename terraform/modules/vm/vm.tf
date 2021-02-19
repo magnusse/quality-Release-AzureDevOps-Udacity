34293aaa58ef12feeaa8239aeda70c51e04e6e6d
magnusse <soenke.magnussen@gmail.com>
2021-02-19 09:36:40 +0100

    last changes

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -21,7 +21,7 @@ resource "azurerm_linux_virtual_machine" "test-vm" {
   network_interface_ids = [azurerm_network_interface.test-nintfc.id]
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("C:/Users/magnu/.ssh/id_rsa.pub")
+    public_key = file("/home/vsts/work/_temp/id_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
