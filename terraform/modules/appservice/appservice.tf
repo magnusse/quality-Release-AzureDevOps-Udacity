97c9ec3e2b65b16cd13053563d9ff5e977ab891d
magnusse <soenke.magnussen@gmail.com>
2021-03-08 15:51:16 +0100

    rename webapp

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -11,7 +11,7 @@ resource "azurerm_app_service_plan" "test" {
 }
 
 resource "azurerm_app_service" "test" {
-  name                = "${var.application_type}-SMArtificial"
+  name                = "WebAppSmartificial"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   app_service_plan_id = azurerm_app_service_plan.test.id
